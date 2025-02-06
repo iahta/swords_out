f7bf14fc30a3226aa962a6ad2ff0e47541a4aa8c
iahta <153889016+iahta@users.noreply.github.com>
2025-02-05 20:54:08 -0600

    intro section

--- restart/intro.py
+++ restart/intro.py
@@ -1,18 +1,21 @@
 import os
 import textwrap
 
-def intro():
-    columns, lines = get_terminal_size()
-    print("\033[31mWelcome to Swords Out!\033[0m")
-    print("Swords out is a text-based detective game where you're the detective.")
-    player_name = input("To get started, whats your name?\n")
+
+def intro(columns, player_name):  
     clear_screen()
     start = textwrap.fill(f"{player_name} sits hunched in a corner of their favorite tavern, The Golden Pike, as they drink away their boredom. Two years since a mystery that has really caught their interests. They could only stand to find the farmer's cow so many times.\n", width=columns)
     print(start)
     if input(f"{player_name} drinks their ale \033[33m(type drink)\033[0m \n").lower() == 'drink':
-        print("something something")
-
-
+        print(textwrap.fill(f"The tavern was lively. Lucky, the tavern owner, paraded around his establishment conversing with every patron. He knew not to mess with {player_name}. Especailly when they didn't have a case.", width=columns))
+        if input(textwrap.fill(f"{player_name} took another drink, the ale filling the hole left by their need to investigate. \033[33m(type drink)\033[0m \n", width=columns)).lower() == 'drink':
+            print(textwrap.fill(f"The tavern door burst open. Soldiers clad in shining, clean armor filled the tavern. The leader spoke, 'Where is {player_name}?'"))
+            if input(textwrap.fill(f"{player_name} held their hand up. Finishing their drink. \033[33m(type drink)\033[0m \n")).lower() == 'drink':
+                clear_screen()
+                print(textwrap.fill(f"{player_name} alalalal", width=columns))
+    
+   
+        
 
 
 
@@ -24,11 +27,6 @@ def clear_screen():
     else:
         _ = os.system('clear')
 
-def get_terminal_size():
-    try:
-        size = os.get_terminal_size()
-    except OSError:
-        size = os.terminal_size((80, 24))  # Default size if querying fails
-    return size.columns, size.lines
+
 
 
--- restart/main.py
+++ restart/main.py
@@ -1,9 +1,26 @@
 #from game import game
 from intro import intro
+import os
+
 def main():
-    intro()
+    def get_terminal_size():
+        try:
+            size = os.get_terminal_size()
+        except OSError:
+            size = os.terminal_size((80, 24))  # Default size if querying fails
+        return size.columns, size.lines
+    
+    columns, lines = get_terminal_size()
+    print("\033[31mWelcome to Swords Out!\033[0m\n")
+    print("Swords out is a text-based detective game where you're the detective.")
+    player_name = input("Start detecting, whats your name?\n") #everything above this, needs to go to main
+    ##player name, class creation
+    ##create estate
+    intro(columns, player_name)
     """print("Welcome to Swords Out!")
     if input("Type Start to begin the game: ").lower() != "start":
         game()"""
+    
+    
 if __name__ == "__main__":
     main()
