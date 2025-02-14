b60df56fd6ada6f55eca1f4323c86d8b0a4e8767
iahta <153889016+iahta@users.noreply.github.com>
2025-02-13 19:25:20 -0600

    adding the fluff

--- dialogue.py
+++ dialogue.py
@@ -1,5 +1,6 @@
 import textwrap
 import os
+import time
 
 def print_wrapped(text):
     # Get the width of the terminal dynamically
@@ -7,7 +8,18 @@ def print_wrapped(text):
     # Wrap the text
         wrapped_text = textwrap.fill(text, width)
     # Print the wrapped text
+        time.sleep(1)
         print(wrapped_text)
+
+
+def print_king(text):
+    # Get the width of the terminal dynamically
+        width = os.get_terminal_size().columns
+    # Wrap the text
+        wrapped_text = textwrap.fill(text, width)
+    # Print the wrapped text
+        time.sleep(1)
+        print(f"\033[32m{wrapped_text}\033[0m")
 
 
 
--- npc.py
+++ npc.py
@@ -1,4 +1,4 @@
-from dialogue import print_wrapped
+from dialogue import *
 
 class NPC():
     def __init__(self, name, title):
@@ -7,20 +7,29 @@ class NPC():
         self.suspect = False
         self.talked_to = False
         self.notes = []
+        self.times = 1
 
 class KingIngmar(NPC):
     def __init__(self):
         super().__init__("King Ingmar", "King of the Rothsdale Isles, Leader of Clan Tremere, Governor of Frandale")
+        
 
     def talk(self, player):
-        print("talk to ingmar")
-        note1 = "Note 1"
-        note2 = "Note 2"
-        note3 = "Note 3"
-        notes = [note1, note2, note3]
+        notes = []
+        if self.times == 1:
+            print_king("'Welcome Detective, I need your help. One of these four people gathered in this room is responsible for the death of the Bishop. I am hoping you can tell me who.'")
+            print_wrapped(f"{player.name} takes in the site of of the King. His eyes are heavy with stress, his hair dishelved, a stark contrast to the opulent and pressed green robes that adorn his body.")
+            print_wrapped(f"'I am happy to be of help your highness,' {player.name} says, 'If I may, I have a few questions.'")
+            print_wrapped(f"'Where is the body?'")
+            print_king("The Garden, underneath the elder tree.")
+            notes.append("The body is in the Garden")
+            print_wrapped(f"'And when was he found?'")
+            print_king("'The Bishop was found this morning, just after the sun broke over the horizon, the Gardener found him.'")
+            notes.append("The body was found in the morning.")
+        
+        
         self.notes.extend(notes)
         player.journal[self.name] = self.notes
-        print(player.journal)
         self.talked_to = True
             
         #king has win conditions all evidence
